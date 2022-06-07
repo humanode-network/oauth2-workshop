250901f825cd02c074a450f138a762784972ebfc
Tony C <tonycdot@yahoo.com>
2022-06-07 17:36:47 +0400

    Add codeVerifier in login route

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const openid = require('openid-client');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.set('view engine', 'jade');
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
   const name = 'guest';
@@ -30,6 +32,10 @@ app.get('/login', async (req, res) => {
 
   // Save client configuration for later use.
   res.app.set('client', client);
+
+  // Set up codeVerifier and save it as a cookie for later use.
+  const codeVerifier = openid.generators.codeVerifier(64);
+  res.cookie('codeVerifier', codeVerifier, { maxAge: 360000 });
 });
 
 app.listen(3000, () => {
