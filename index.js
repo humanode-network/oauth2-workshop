5ab648d65f9f5e25117ac0db35792547a09269c4
Tony C <tonycdot@yahoo.com>
2022-06-07 17:24:27 +0400

    Add root route

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.set('view engine', 'jade');
+
+app.get('/', (req, res) => {
+  const name = 'guest';
+
+  res.render('index', {
+    name
+  });
+});
+
 app.listen(3000, () => {
   console.log('App listening on port 3000!');
 });
