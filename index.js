4e7a86bccf518e2a16d8b1117ab8bb7fe11b3503
Tony C <tonycdot@yahoo.com>
2022-06-07 17:32:00 +0400

    Add login route with Issuer and Client setup.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const openid = require('openid-client');
 
 const app = express();
 
@@ -10,6 +11,25 @@ app.get('/', (req, res) => {
   res.render('index', {
     name
   });
+});
+
+app.get('/login', async (req, res) => {
+  // Humanode Issuer a.k.a. Identity provider.
+  const humanodeIssuer = await openid.Issuer.discover(
+    'https://auth.staging.oauth2.humanode.io'
+  );
+
+  // Set up the common hackathon client.
+  const client = new humanodeIssuer.Client({
+    client_id: 'hackathon-participant',
+    client_secret: 'q4_GkveX47i3M9wYXSkU5CKn3h',
+    redirect_uris: ['http://localhost:3000/callback'],
+    response_types: ['code'],
+    token_endpoint_auth_method: 'client_secret_post'
+  });
+
+  // Save client configuration for later use.
+  res.app.set('client', client);
 });
 
 app.listen(3000, () => {
